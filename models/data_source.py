d4e12efd895c1fcf4ff0fbf7f59556211a329cce
bwyrzykowski1 <163434905+bwyrzykowski1@users.noreply.github.com>
2024-04-16 18:52:03 +0200

    Revert "wip: add new menu function, add user append"
    This reverts commit 80a0de27ca0fbe5941d94f88d031ce7bbd6d4df1.

--- models/data_source.py
+++ models/data_source.py
@@ -3,5 +3,5 @@ users: list = [
     {'name': 'Julia', 'surname': 'Gotowiec', 'posts': '1500'},
     {'name': 'Hubert', 'surname': 'Sybilski', 'posts': '123456789'},
     {'name': 'Adrian', 'surname': 'Dobrzański', 'posts': '123'},
-    {'name': 'Bartek', 'surname': 'Wyrzykowski', 'posts': '300'},
+
 ]

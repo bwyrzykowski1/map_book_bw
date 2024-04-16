80a0de27ca0fbe5941d94f88d031ce7bbd6d4df1
bwyrzykowski1 <163434905+bwyrzykowski1@users.noreply.github.com>
2024-04-16 18:51:09 +0200

    wip: add new menu function, add user append

--- utils/crud.py
+++ utils/crud.py
@@ -8,3 +8,8 @@ def add_user(lista: list) -> None:
     liczba_postow = int(input('Podaj liczbę postów użytkownika: '))
     new_user = {'name': imie, 'surname': nazwisko, 'posts': liczba_postow},
     lista.append(new_user)
+def search_user(users: list):
+    imie = input('Podaj imie: ')
+    for user in users:
+        if user['name'] == imie:
+            print(user)

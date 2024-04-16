f074a49fadc60124ba4efa1989d6ee362b716d36
bwyrzykowski1 <163434905+bwyrzykowski1@users.noreply.github.com>
2024-04-16 18:53:09 +0200

    wip: add new menu function, add user append, user search

---
+++ notatnik.py
@@ -0,0 +1,13 @@
+users: list = [
+
+    {'name': 'Julia', 'surname': 'Gotowiec', 'posts': '1500'},
+    {'name': 'Hubert', 'surname': 'Sybilski', 'posts': '123456789'},
+    {'name': 'Adrian', 'surname': 'Dobrzański', 'posts': '123'},
+    {'name': 'Bartek', 'surname': 'Wyrzykowski', 'posts': '300'},
+]
+def search_user(users: list):
+    imie = input('Podaj imie: ')
+    for user in users:
+        if user['name'] == imie:
+            print(user)
+search_user(users)
